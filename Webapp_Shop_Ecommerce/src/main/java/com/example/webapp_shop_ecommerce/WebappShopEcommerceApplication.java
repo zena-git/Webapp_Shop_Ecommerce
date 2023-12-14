0a2c700d064262cc2e0e2768148153e9efc7bc92
zena-git <chienlai2k3@hotmail.com>
2023-12-14 23:40:40 +0700

    create proj

---
+++ Webapp_Shop_Ecommerce/src/main/java/com/example/webapp_shop_ecommerce/WebappShopEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp_shop_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappShopEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappShopEcommerceApplication.class, args);
+    }
+
+}
