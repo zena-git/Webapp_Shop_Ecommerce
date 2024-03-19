b5288ffd6961ad2df1604f2af6ebf2856799c065
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-03-19 15:51:03 +0700

    khách hàng

--- webapp_shop_ecommerce_fe/src/pages/Customer/index.js
+++ webapp_shop_ecommerce_fe/src/pages/Customer/index.js
@@ -1,7 +1,35 @@
-function Customer() {
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { baseUrl } from '~/lib/functional'
+import ListTable from '../../components/voucher/listCustomer'
+import ReduxProvider from '../../redux/provider'
+
+const VoucherPage = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${baseUrl}/voucher`).then(res => {
+            setData(res.data);
+        })
+    }, [])
+
     return (
-        <h1>Customer</h1>
-    );
+        <div className="py-6">
+            <p className='text-xl font-bold'>Khách hàng</p>
+            <div>
+                <div className='mt-5'>
+                    <ListTable data={data} />
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
-export default Customer;
+const Layout = (props) => {
+    return (
+        <ReduxProvider><VoucherPage></VoucherPage></ReduxProvider>
+    )
+}
+
+export default Layout
